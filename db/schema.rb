c895f3d6810f5c9dad886c996aba9f4117c985b7
Ehab Ahmed <ehabahmed2456@gmail.com>
2024-01-19 02:01:26 -0500

    fixed user auth

--- db/schema.rb
+++ db/schema.rb
@@ -53,13 +53,6 @@ ActiveRecord::Schema.define(version: 2023_12_22_170757) do
     t.index ["category_id"], name: "index_products_on_category_id"
   end
 
-  create_table "users", force: :cascade do |t|
-    t.string "name"
-    t.string "email"
-    t.string "password_digest"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-  end
 
   add_foreign_key "line_items", "orders"
   add_foreign_key "line_items", "products"
